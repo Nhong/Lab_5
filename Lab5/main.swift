f31880b54f084f525af1b1a81d65c3b6c40ea12b
kittinun chobthum <chobtham.kittinun@gmail.com>
2020-04-04 13:44:59 +0700

    Initial Commit

---
+++ Lab5/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lab5
+//
+//  Created by Kittinun Chobtham on 4/4/2563 BE.
+//  Copyright © 2563 Kittinun Chobtham. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
